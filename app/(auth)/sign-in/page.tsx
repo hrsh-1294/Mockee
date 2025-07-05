081c88c641dac571842476136a2c6586dd31f3ee
hrsh-1294 <harshvashishth12@gmail.com>
2025-07-05 19:40:45 +0530

    proper routing and created auth pages

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,8 @@
+import AuthForm from '@/app/components/AuthForm'
 import React from 'react'
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
+  return <AuthForm type="sign-in" />
 }
 
 export default page
