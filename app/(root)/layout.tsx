239d1baf686a22665f02c7daa9696ada1ffcfe95
hrsh-1294 <harshvashishth12@gmail.com>
2025-07-07 16:45:01 +0530

    developed UI of the home page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,19 @@
+import Image from 'next/image'
+import  Link  from 'next/link'
 import { ReactNode } from 'react'
 
 const Rootlayout = ({children} : {children:ReactNode}) => {
   return (
-    <div>{children}</div>
+    <div className='root-layout'>
+      <nav>
+        <Link href="/" className='flex items-center gap-2'>
+          <Image src="/logo.svg" alt="Mockee Logo" width={38} height={32} />
+          
+          <h2 className='text-primary-100'>Mockee</h2>
+        </Link>
+      </nav>
+      {children}
+    </div>
   )
 }
 
